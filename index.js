8185b1e7d92f9d1f1d769640ce96143af903fe69
Vitalii <kicker198686@gmail.com>
2020-09-07 23:15:58 +0300

    first

---
+++ index.js
@@ -0,0 +1,3 @@
+let mac = 100;
+const mac2 = 200;
+console.log(mac, mac2);
---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log(5 + " ");
+
+let i = 10,
+  y = 10;
+console.log(888 * 111);
+console.log(y--);
